313d8d2ba80f1c217f2ddd228e9a96c60da926a3
loxygenk <me@loxygen.dev>
2021-10-17 16:04:09 +0900

    🚨 Run linter

--- lambda/packages/authenticateGAuth/src/main.ts
+++ lambda/packages/authenticateGAuth/src/main.ts
@@ -1,23 +1,26 @@
 import isEven from "is-even";
-import {APIGatewayEventRequestContext, APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import {
+  APIGatewayEventRequestContext,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from "aws-lambda";
 
 export async function handler(
   event: APIGatewayEventRequestContext,
   context: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
-
   const randomNumber = Math.floor(Math.random() * 1000000);
 
   const response = {
-    "message": "Beep-poop, here is authenticateGAuth.",
-    "additional": {
-      "number": randomNumber,
-      "even": isEven(randomNumber)
+    message: "Beep-poop, here is authenticateGAuth.",
+    additional: {
+      number: randomNumber,
+      even: isEven(randomNumber),
     },
   };
 
   return {
     statusCode: 200,
-    body: JSON.stringify(response)
+    body: JSON.stringify(response),
   };
 }
