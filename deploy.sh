a94a9235de7bc77f44b6fe94c146d85568e02a61
Flisan <loxygen.k@gmail.com>
2021-10-17 10:52:14 +0900

    👷 Configure the workflow to deploy Lambda function (#2)

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+# Lambda function deploy script.
+# From https://github.com/blombard/lambda-monorepo
+
+FUNCTION_NAME=$1
+PATH_NAME=$2
+ZIP_PARAMS=$3
+
+echo "==> Deploying $FUNCTION_NAME (at $PATH_NAME)"
+
+if [ -n "$PATH_NAME" ]; then cd $PATH_NAME; fi
+
+echo "    ==> Installing dependencies"
+yarn install --frozen-lockfile
+if [ $? -eq 1 ]; then
+  echo "  ==> Installing dependencies - FAILED"
+  exit 1
+fi
+
+echo "    ==> Creating layer ZIP file"
+zip lambda.zip -r $ZIP_PARAMS
+
+export AWS_LAMBDA_DEPLOY_FAILURE=0
+
+echo "    ==> Deploying code"
+aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://lambda.zip || export AWS_LAMBDA_DEPLOY_FAILURE=1
+
+echo "    ==> Deploying environment variables"
+aws lambda update-function-configuration --function-name $FUNCTION_NAME --environment Variables="{`cat .env | xargs | sed 's/ /,/g'`}" || export AWS_LAMBDA_DEPLOY_FAILURE=1
+
+rm -f lambda.zip
+
+exit $AWS_LAMBDA_DEPLOY_FAILURE
