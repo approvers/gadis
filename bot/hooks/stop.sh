a499fd4e732360d82498452153beb2cfcb6c0aaf
loxygenK <me@loxygen.dev>
2021-10-19 09:45:35 +0900

    👷(bot) Back up the executable before installation

--- bot/hooks/stop.sh
+++ bot/hooks/stop.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 pkill gadis_bot || echo "[Warning] Killing gadis_bot failed; ignoring exit code"
+mkdir /home/ec2-user/bot/executable_backups
+
+PREFIX="$(date '+%Y%m%d-%H%M%S')-$RANDOM" mv /home/ec2-user/bot/gadis_bot /home/ec2/bot/executable_backups/$PREFIX-gadis_bot
