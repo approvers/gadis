a4a8d99760e932a7f58adecd7d8e234386288fa8
loxygenK <me@loxygen.dev>
2021-10-21 13:43:30 +0900

    🚀(bot) Skip creating directory if it was created

--- bot/hooks/stop.sh
+++ bot/hooks/stop.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 pkill gadis_provider || echo "[Warning] Killing gadis_bot failed; ignoring exit code"
-mkdir /home/ec2-user/bot/executable_backups
+if [ ! -d /home/ec2-user/bot/executable_backups ]; then
+  mkdir /home/ec2-user/bot/executable_backups
+else
+  echo "Directory was already created."
+fi
 
-PREFIX="$(date '+%Y%m%d-%H%M%S')-$RANDOM" mv /home/ec2-user/bot/gadis_provider /home/ec2/bot/executable_backups/$PREFIX-gadis_provider
+PREFIX="$(date '+%Y%m%d-%H%M%S')-$RANDOM"
+mv /home/ec2-user/bot/gadis_provider /home/ec2/bot/executable_backups/$PREFIX-gadis_provider
