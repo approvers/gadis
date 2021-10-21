4b318f11518ed6dd4cb69b3afcdd75e47358e7ac
loxygenK <me@loxygen.dev>
2021-10-21 14:11:48 +0900

    🚀(bot) Skip backup when the executable was not found

--- bot/hooks/stop.sh
+++ bot/hooks/stop.sh
@@ -7,5 +7,9 @@ else
   echo "Directory was already created."
 fi
 
-PREFIX="$(date '+%Y%m%d-%H%M%S')-$RANDOM"
-mv /home/ec2-user/bot/gadis_provider /home/ec2/bot/executable_backups/$PREFIX-gadis_provider
+if [ -f /home/ec2-user/bot/gadis_provider ]; then
+  PREFIX="$(date '+%Y%m%d-%H%M%S')-$RANDOM"
+  mv /home/ec2-user/bot/gadis_provider /home/ec2/bot/executable_backups/$PREFIX-gadis_provider
+else
+  echo "No executables found."
+fi
