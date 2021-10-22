cb3a34a4da0c4d589e791044731efd68f9af86c8
loxygenK <me@loxygen.dev>
2021-10-22 11:48:33 +0900

    💩(bot/api) Create dummy struct for resolving error temporality

--- bot/gadis_provider/gadis_api/src/setup.rs
+++ bot/gadis_provider/gadis_api/src/setup.rs
@@ -1,3 +1,13 @@
-pub(crate) fn initialize() {
-    todo!("Initialize required things");
+use async_trait::async_trait;
+use crate::controller::UserRequestHandler;
+
+struct Dummy;
+
+#[async_trait]
+impl UserRequestHandler for Dummy {
+    async fn start(self) { unimplemented!(); }
+}
+
+pub(crate) fn initialize() -> impl UserRequestHandler {
+    Dummy
 }
