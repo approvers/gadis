31884c960f26ebd62ebe32edbae20a5d8f174eba
loxygenK <me@loxygen.dev>
2021-10-22 12:01:50 +0900

    🔖(bot/bot) Create User/Role entity for gadis_bot

--- bot/gadis_provider/gadis_bot/src/entity.rs
+++ bot/gadis_provider/gadis_bot/src/entity.rs
@@ -1,3 +1,11 @@
 pub struct User {
+    pub name: String,
+    pub nickname: String,
+    pub pic_url: String,
+    pub roles: Vec<Role>
+}
 
+pub struct Role {
+    pub name: String,
+    pub color_code: u16
 }
