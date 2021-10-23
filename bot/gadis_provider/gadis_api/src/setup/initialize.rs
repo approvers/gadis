cecebb6ff250cbd45f57e0008422f4977446b8e2
loxygenk <me@loxygen.dev>
2021-10-23 17:59:07 +0900

    ✨ Return appropriate client

--- bot/gadis_provider/gadis_api/src/setup/initialize.rs
+++ bot/gadis_provider/gadis_api/src/setup/initialize.rs
@@ -2,7 +2,14 @@ use async_trait::async_trait;
 use crate::setup::Start;
 
 use crate::controller::warp::HttpUserRequestHandler;
+use crate::controller::console::ConsoleUserRequestHandler;
 
 pub(crate) fn initialize() -> impl Start {
-    HttpUserRequestHandler
+    #[cfg(feature = "client-warp")]
+    let client = HttpUserRequestHandler;
+
+    #[cfg(feature= "client-console")]
+    let client = ConsoleUserRequestHandler;
+
+    client
 }
